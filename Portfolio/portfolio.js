0fe44bf15c53f1848c54ac84a4eaac58b9c993b2
nicholasdwebb <nicholasdwebb@me.com>
2017-06-04 11:49:33 -0400

    Add files via upload

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -3,6 +3,7 @@ var iphone = $("#iphone");
 var ipH = iphone.height();
 var ww = window.innerWidth;
 var scaled;
+var resume = getVariable("resume");
 // var originalOffset;
 // var orginalHeight;
 $(function(){
@@ -96,4 +97,14 @@ function waDemo(){
 		back.width(window.innerWidth);
 		back.height(back.width()*3/4);
 	}
+}
+function getVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
 }
